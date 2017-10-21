3d778742402e9aed757de563e975cfcf01d246b2
Rafal Stapinski <stapinskirafal@gmail.com>
2017-10-21 14:36:29 -0400

    start grayscale

--- sobel/sobel.py
+++ sobel/sobel.py
@@ -1,9 +1,42 @@
 from PIL import Image
 
-class Sobel:
+class Open:
 
     def __init__(self, image_path):
 
         self.image = Image.open(image_path)
+        self.width, self.height = self.image.size
+        self.raw = self.image.load()
+        self.data = [[None for x in range(self.width)] for y in range(self.height)]
 
-        self.image.show()
+    @staticmethod
+    def luminosity(rgb):
+        return (.21 * rgb[0]) + (.72 * rgb[1]) + (.07 * rgb[2])
+
+    @staticmethod
+    def average(rgb):
+        return sum(rgb) / 3
+
+    @staticmethod
+    def lightness(rgb):
+        return (max(rgb) + min(rgb)) / 2
+
+    def grayscale(self, mode=None):
+
+        if mode == None:
+            mode = 'lum'
+
+        if mode == 'lum':
+            scale = self.luminosity
+        elif mode == 'avg':
+            scale = self.average
+        elif mode == 'lht':
+            scale = self.lightness
+        else:
+            pass
+            # TODO: error handling
+
+        for x in xrange(1, self.width - 1):
+            for y in xrange(1, self.height - 1):
+                self.data[x, y] = scale(self.raw[x ,y])
+                print self.data[x, y]
